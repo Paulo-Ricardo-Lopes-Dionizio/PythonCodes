e5ab39b6bd45077df133505f6f025a56531ba7ec
Paulo Ricardo Lopes Dionizio <66482705+Paulo-Ricardo-Lopes-Dionizio@users.noreply.github.com>
2023-12-03 21:41:45 -0300

    Update VendedoresDinamicos.py

--- VendedoresDinamicos.py
+++ VendedoresDinamicos.py
@@ -1,19 +1,26 @@
 N = int(input('Digite a quantidade de vendedores\n'))
 M = int(input('Digite a quantidade de item por vendedor\n'))
-linhas = N
-colunas = M
-vendedores = [0] * N
-itens = [0] * M
-valor = [0] * M
+
+repetir = 'finalizar'
+comercio = []
 
 for i in range(0,N):
-    vendedores[i] = str(input('Quais os nomes dos vendedores? : \n'))
+    vendedores = {}
+    vendedores['nome'] = input(f'Digite o nome do vendedor {i + 1}: ')
+   # vendedores['nomes'] = []
+    vendedores['itens'] = []
+    vendedores['valores'] = []
     for j in range(0, M):
-        itens[j] = str(input('Quais os itens dos vendedores? : \n'))
-        valor[j] = float(input('Quais os valores dos vendedores?\n'))
-    print(vendedores)
-    print(f'{itens, valor}')
-
-
-
+        #vendedor = input(f'Quais os nomes dos vendedores {j + 1} ?: \n')
+        item = input(f'Digite o nome do item {j + 1} vendido pelo vendedor {vendedores["nome"]}?:\n ')
+        valor = float(input(f'Digite o valor do item {j + 1} vendido pelo vendedor {vendedores["nome"]}:\n '))
+        #vendedores['nomes'].append(vendedor)
+        vendedores['itens'].append(item)
+        vendedores['valores'].append(valor)
+    comercio.append(vendedores)
+print(comercio)
+#print(f'Senhores: {vendedor}')
+while repetir == 'finalizar':
+    comercio[i] = input(f'Digite qual Vendedor deseja comprar? ou Finalizar\n')
+    print(f'Você escolheu o {comercio[i]}\n os itens e valores são : ({item} - {valor})')
 
