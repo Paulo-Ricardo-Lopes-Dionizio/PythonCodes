a1ea03bc35d9448d3d1ff019d61b3fc1715333be
Paulo Ricardo Lopes Dionizio <66482705+Paulo-Ricardo-Lopes-Dionizio@users.noreply.github.com>
2023-12-03 23:32:22 -0300

    Finalização PythonCodes 1.0
    Futuras Atualizações em breve para otimizar e deixar mais editável.

--- README.md
+++ README.md
@@ -1,11 +1,79 @@
-# PythonCodes
+# PythonCodigos | PythonCodes | プログラムコード パイソン
 Aqui terá alguns códigos e projetos feitos por mim com o meu tempo livre
  - Aqui terá alguns Projetos feitos por mim para estudo, programas mais simples estarão de forma livre, enquanto programas mais complexo estarão em pastas com seus arquivos dentro. 
  - Here you will have some Projects made by me for study, simpler programs will be free, while more complex programs will be in folders with their files inside.
  - ここでは私が研究のために作成したプロジェクトをいくつか紹介します。単純なプログラムは無料ですが、より複雑なプログラムはファイルのあるフォルダーにあります。
 
 <h1>VendedoresDinamicos</h1>
- - Registro unitario( em processo) 03/12/2023
+ - Finalizado 03/12/2023
+ - Vantagens:
 
+   - Multiplos vendedores, itens, valores
+   - Identificar Vendedores
+
+ - Desvantagens:
+
+   - Compras apenas 1 por vez
+   - Quantidade de itens entre todos os vendedores são limitados
+ 
 <h1>VendedoresEstaticos</h1>
  - Finalizado 02/12/2023
+ - Vantagens:
+
+   - Escolher qual item cada vendedor terá
+   - Somar todos os valores de compra por cada vendedor no final
+   - Utilização apenas de "if/else","while" 
+
+ - Desvantagens:
+   - Vendedores limitados
+   - Valores Limitados
+/----------------------------------------------------------------------------------------------------------------------/
+<h1>Dynamic Sellers</h1>
+ - Completed 12/03/2023
+ - Benefits:
+
+   - Multiple sellers, items, values
+   - Identify Sellers
+
+ - Disadvantages:
+
+   - Purchase only 1 at a time
+   - Quantity of items among all sellers are limited
+ 
+<h1>Static Salespeople</h1>
+
+ - Completed 12/02/2023
+ - Benefits:
+
+   - Choose which item each seller will have
+   - Add up all purchase values for each seller at the end
+   - Use only "if/else","while"
+
+ - Disadvantages:
+   - Limited sellers
+   - Limited Values
+/----------------------------------------------------------------------------------------------------------------------/
+<h1>売り手</h1>
+ - 2023/12/03 完了
+ - 利点:
+
+   - 複数の販売者, アイテム, 価値観
+   - 販売者の特定
+
+ - 短所:
+
+   - 一度に1つだけ購入してください
+   - すべての販売者の商品の数量には限りがあります
+ 
+<h1>売り手</h1>
+
+ - 2023/12/02 完了
+ - 利点:
+
+   - 各販売者がどのアイテムを所有するかを選択してください
+   - 最後に各販売者のすべての購入額を合計します。
+   - 「if/else」、「while」のみを使用してください
+
+ - 短所:
+   - 限定販売者
+   - 限られた価値観
--- VendedoresDinamicos.py
+++ VendedoresDinamicos.py
@@ -1,9 +1,8 @@
 N = int(input('Digite a quantidade de vendedores(as)\n'))
 M = int(input('Digite a quantidade de item por vendedor(a)\n'))
-
 comercio = []
+repetir = "não"
 repeticao = 0
-
 for i in range(0,N):
     vendedores = {}
     vendedores['nome'] = input(f'Digite o nome do(a) vendedor(a) {i + 1}: ')
@@ -12,29 +11,30 @@ for i in range(0,N):
     for j in range(0, M):
         item = input(f'Digite o nome do item {j + 1} vendido pelo(a) vendedor(a) {vendedores["nome"]}?:\n ')
         valor = float(input(f'Digite o valor do item {j + 1} vendido pela(a) vendedor(a) {vendedores["nome"]}:\n '))
-
         vendedores['itens'].append(item)
         vendedores['valores'].append(valor)
     comercio.append(vendedores)
-print(comercio)
+valor_total = 0
 while True:
+    print(comercio)
     nome_vendedor = input('Qual o(a) vendedor(a) que você deseja comprar ou digite "Finalizar" para sair:\n ')
     if nome_vendedor.lower() == 'finalizar':
         break
     for vendedor in comercio:
         if vendedor['nome'] == nome_vendedor:
             print(f'Você escolheu o(a) vendedor(a) {vendedor["nome"]}. Os itens e valores são: {vendedor["itens"]} - {vendedor["valores"]}')
-            #if repeticao == 0:
-                #confimar = input('Deseja comprar esse item?\n')
-           # else:
-               # confimar = input('Deseja comprar novamente esse item?\n')
-
-            #if confimar == 'sim':
-                #repeticao = repeticao + 1
-                #vendedor["valor"] = vendedor["valor"]
-            #else:
-                #trocar = input('deseja trocar de vendedor?\n')
-                #break
+            while repetir == "não":
+                if repeticao == 0:
+                    confimar = input('Deseja comprar esse item?\n')
+                else:
+                    confimar = input('Deseja comprar novamente esse item?\n')
+                if confimar == 'sim':
+                    repeticao = repeticao + 1
+                    valor_total += sum(vendedor['valores'])
+                else:
+                    trocar = input('deseja trocar de vendedor?\n')
+                    break
+            print(f'O valor total do(a) vendedor(a) {vendedor["nome"]} é R${valor_total:.2f}')
             break
     else:
         print(f'O(a) vendedor(a) "{nome_vendedor}" não foi encontrado(a). Tente novamente.')
