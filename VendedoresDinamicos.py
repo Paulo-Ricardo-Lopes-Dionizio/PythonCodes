f52186bbfb451f1c28892806b7ad520f9e52b820
Paulo Ricardo Lopes Dionizio <66482705+Paulo-Ricardo-Lopes-Dionizio@users.noreply.github.com>
2023-12-03 22:23:28 -0300

    Update VendedoresDinamicos.py
    Atualização, sistema de escolha de vendedor

--- VendedoresDinamicos.py
+++ VendedoresDinamicos.py
@@ -1,26 +1,29 @@
-N = int(input('Digite a quantidade de vendedores\n'))
-M = int(input('Digite a quantidade de item por vendedor\n'))
+N = int(input('Digite a quantidade de vendedores(as)\n'))
+M = int(input('Digite a quantidade de item por vendedor(a)\n'))
 
-repetir = 'finalizar'
 comercio = []
 
 for i in range(0,N):
     vendedores = {}
-    vendedores['nome'] = input(f'Digite o nome do vendedor {i + 1}: ')
+    vendedores['nome'] = input(f'Digite o nome do(a) vendedor(a) {i + 1}: ')
    # vendedores['nomes'] = []
     vendedores['itens'] = []
     vendedores['valores'] = []
     for j in range(0, M):
-        #vendedor = input(f'Quais os nomes dos vendedores {j + 1} ?: \n')
-        item = input(f'Digite o nome do item {j + 1} vendido pelo vendedor {vendedores["nome"]}?:\n ')
-        valor = float(input(f'Digite o valor do item {j + 1} vendido pelo vendedor {vendedores["nome"]}:\n '))
-        #vendedores['nomes'].append(vendedor)
+        item = input(f'Digite o nome do item {j + 1} vendido pelo(a) vendedor(a) {vendedores["nome"]}?:\n ')
+        valor = float(input(f'Digite o valor do item {j + 1} vendido pela(a) vendedor(a) {vendedores["nome"]}:\n '))
+
         vendedores['itens'].append(item)
         vendedores['valores'].append(valor)
     comercio.append(vendedores)
 print(comercio)
-#print(f'Senhores: {vendedor}')
-while repetir == 'finalizar':
-    comercio[i] = input(f'Digite qual Vendedor deseja comprar? ou Finalizar\n')
-    print(f'Você escolheu o {comercio[i]}\n os itens e valores são : ({item} - {valor})')
-
+while True:
+    nome_vendedor = input('Qual o(a) vendedor(a) que você deseja comprar ou digite "Finalizar" para sair:\n ')
+    if nome_vendedor.lower() == 'finalizar':
+        break
+    for vendedor in comercio:
+        if vendedor['nome'] == nome_vendedor:
+            print(f'Você escolheu o(a) vendedor(a) {vendedor["nome"]}. Os itens e valores são: {vendedor["itens"]} - {vendedor["valores"]}')
+            break
+    else:
+        print(f'O(a) vendedor(a) "{nome_vendedor}" não foi encontrado(a). Tente novamente.')
