eb436415f05f6f2884698c41b2de5cd9d9bead2a
Paulo Ricardo Lopes Dionizio <66482705+Paulo-Ricardo-Lopes-Dionizio@users.noreply.github.com>
2024-02-19 11:21:00 -0300

    Update ProjetoJanela.py

--- ProjetoJanela.py
+++ ProjetoJanela.py
@@ -6,9 +6,19 @@ class teste1:
         self.msg = Label(self.widget1,text='Guinando')
         self.msg.pack()
 janela = Tk()
-teste1(janela)
-janela.geometry('300x300')
-botao = Button(janela, width=20,text='botao1')
-botao.place(x=80,y=140)
 janela.title('teste')
+janela.geometry('250x250')
+#teste1(janela)coloca o texto em posiçao inicial na parte superior no centro
+#botao.place(x=80,y=140) posição em qualquer ligar sem quadro de responsividade
+
+label = Label(janela, width=15,height=2,text='texto apresentado',relief='flat',fg='#fcb603',bg='#121211')
+label.grid(row=1,column=0,padx=10,pady=10)
+
+botao = Button(janela, width=10,height=1,text='botao1',relief='flat',fg='#fcb603',bg='#121211')
+botao.grid(row=0,column=0,padx=10,pady=10)
+botao2 = Button(janela, width=10,height=1,text='botao2',relief='flat',fg='#fcb603',bg='#121211')
+botao2.grid(row=0,column=1,padx=10,pady=10)
+
+
+
 janela.mainloop()
